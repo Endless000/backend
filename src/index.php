d6a3c52ade7811c53b0051b88ccbc26584458730
WWW <iseepeople@protonmail.com>
2019-10-31 18:45:51 +0200

    homowork LMS 6

--- src/index.php
+++ src/index.php
@@ -1,4 +1,22 @@
 <?php
-$abc = "Anton";
-$cba = 12345;
-require_once ('index.phtml');
+$login = 'Anton';
+$pass = 'password';
+$languages = [
+    'EN' => 'Английский',
+    'UA' => "Украинский",
+    'RU' => "Русский"
+];
+?>
+<form action="index.php" method="POST">
+    <label for="">Login</label>
+    <input type="text" name="login" value="<?=$login?>">
+    <label for="">Password</label>
+    <input type="text" name="pass" value="<?=$pass?>">
+    <input type="submit">
+    <select name="" id="">
+    <?php foreach ($languages as $key => $language) { ?>
+            <option value="<?php echo $key ?>"><?php echo $language ?></option>
+        <?php } ?>
+    </select>
+</form>
+
