380755dfd777a18a1ad6393aab76b995fd381297
WWW <iseepeople@protonmail.com>
2019-10-31 11:34:22 +0200

    homework1

--- Homework php/homework.php
+++ Homework php/homework.php
@@ -1,18 +1,10 @@
 <?php
 $workers = [];
- $workers[1] = ["name" => "Misha", "email" => "misha@test.com"];
- $workers[2] = ["name" => "Bodya", "email" => "bodya@gmail.com"];
- $workers[3] = ["name" => "Irakli", "email" => "irakli@gmail.com"];
- $workers[4] = ["name" => "Roman", "email" => "roman@gmail.com"];
- $workers[5] = ["name" => "Anton", "email" => "anton@gmail.com"];
+ $workers[1] = ["name" => "Misha", "email" => "misha@test.com","lang"=>"en"];
+ $workers[2] = ["name" => "Bodya", "email" => "bodya@gmail.com","lang"=>"ru"];
+ $workers[3] = ["name" => "Irakli", "email" => "irakli@gmail.com","lang"=>"fr"];
+ $workers[4] = ["name" => "Roman", "email" => "roman@gmail.com","lang"=>"ua"];
+ $workers[5] = ["name" => "Anton", "email" => "anton@gmail.com","lang"=>"de"];
  $worker = ["name" => "Misha"];
   krsort($workers);
    unset($workers[1]);
-   echo $workers. '<pre>';
-   echo "max_id = ", current($workers[5]), "<br>";
-   echo "min_id = ", current($worker), "<br>";
-   echo "next_max_id = ", current($workers[4]), "<br>";
-   echo "next_min_id = ", current($workers[2]), "<br>";
-   echo "Количество пользователей  сайта = ". count($workers). '<br>';
-    print_r($workers);
-?>
