14431bd2d61ac7927c3bba56bbf3e9f2b8d1279d
Endless000 <56690264+Endless000@users.noreply.github.com>
2019-10-31 16:24:48 +0200

    Delete index.php

--- src/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-$abc = "Anton";
-$cba = 12345;
-require_once('index.html');
-echo "  Login  <input type='text' name='login' value='".$abc."'>";
-echo "  Password  <input type='text' name='pass' value='".$cba."'>";
