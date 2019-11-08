303f8937ec383447ddde92e477280e20ca2d66d0
WWW <iseepeople@protonmail.com>
2019-11-08 17:41:49 +0200

    task_8 update

--- src/index.php
+++ src/index.php
@@ -1,5 +1,6 @@
 <?php
-$worker[1] = ["name" => "Misha", "email" => "misha@test.com", "lang"=> "ru"];
+$workers = [];
+$worker[1] = ["name" => "Misha", "email" => "misha@test.com", "lang"=> "en"];
 $worker[2] = ["name" => "Bodya", "email" => "bodya@gmail.com", "lang"=> "eu"];
 $worker[3] = ["name" => "Irakli", "email" => "irakli@gmail.com", "lang"=> "de"];
 $worker[4] = ["name" => "Roman", "email" => "roman@gmail.com", "lang"=> "fr"];
@@ -8,8 +9,10 @@ $worker[6] = ["name" => "Misha", "email" => "misha@test.com", "lang"=> "de"];
 $worker[7] = ["name" => "Bodya", "email" => "bodya@gmail.com", "lang"=> "ua"];
 $worker[8] = ["name" => "Irakli", "email" => "irakli@gmail.com", "lang"=> "ru"];
 $worker[9] = ["name" => "Roman", "email" => "roman@gmail.com", "lang"=> "eu"];
-$worker[10] = ["name" => "Anton", "email" => "anton@gmail.com", "lang"=> "en"];
-if ($worker[1]["lang"] == $worker[10]["lang"] ){
+$worker[10] = ["name" => "Anton", "email" => "anton@gmail.com", "lang"=> "ru"];
+$a_worker = current($worker)["lang"];
+$b_worker = end($worker)["lang"];
+if ($a_worker == $b_worker){
     echo 'Привет';
 } else {
     echo 'Привет, Hello ';
